c61bee60bd57f9ce559a7f17aec4c9596d7e7a59
Brenda Waiyego <waiyegobrenda99@gmail.com>
2024-03-15 10:28:16 +0300

    Completed

---
+++ data_types.dart
@@ -0,0 +1,21 @@
+void main() {
+  // Integer (int) data type
+  int age = 21;
+  print('Age: $age');
+
+  //Double data type
+  double price = 10.55;
+  print('Price is: $price');
+
+  // String data type
+  String name = 'Brenda Kinyanjui';
+  print('Name: $name');
+
+  //List data type
+  List<String> towns = ['Nairobi', 'Kisumu', 'Mombasa', 'Eldoret'];
+  print('The major airports are located in $towns');
+
+  //Map data type
+  Map<String, String> capitals = {'Kenya': 'Nairobi', 'Tanzania': 'Daresalaam'};
+  print('Capital cities: $capitals');
+}
